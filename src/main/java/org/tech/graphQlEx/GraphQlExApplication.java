dabb3e0bd0b3ab1a69970707cba1164162d74c4c
Sergei Sidorov <se902i@gmail.com>
2024-05-12 13:52:06 +0300

    Initial commit

---
+++ src/main/java/org/tech/graphQlEx/GraphQlExApplication.java
@@ -0,0 +1,13 @@
+package org.tech.graphQlEx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlExApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraphQlExApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/tech/graphQlEx/GraphQlExApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tech.graphQlEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
